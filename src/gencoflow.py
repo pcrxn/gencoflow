b0049d85f862d0156fe37eb8ce8d1ae5c3477e1d
pcrxn <pcrxn@protonmail.com>
2023-04-06 14:56:10 -0400

    Change comments and docstrings
    Create better docstrings for the script and functions

--- src/gencoflow.py
+++ src/gencoflow.py
@@ -5,16 +5,18 @@ gencoflow.py
 
 Wrapper script for 'parse_gbk.py', 'create_windows.py' and 'vis_windows.R'.
 
-Parse a Prokka-outputted GenBank file of genomic features into a TSV file 
-containing only ORF annotations for downstream use in GenCoFlow.
+Create visualizations of windows surrounding gene targets of interest using
+Prokka output and custom target annotations.
 
-Create target windows using target annotations and parsed Prokka output.
+Dependencies: 
+    - python=3.11.0
+        - biopython=1.80, pandas=1.5.2
+    - R=4.2.3
+        - ggtree=3.6.2
+        - IRanges=2.32.0
+        - thacklr=0.0.0.9000
+        - gggenomes=0.9.5.9000
 
-# TODO: better description
-Create output figure
-
-# TODO: add R dependencies
-Dependencies: python=3.11.0, biopython=1.80, pandas=1.5.2, 
 Other package versions may work but are untested.
 """
 
@@ -33,6 +35,9 @@ from src.version import __version__
 from parse_gbk import main as parse_gbk_main
 from create_windows import main as create_windows_main
 
+#-------------------------------------------------------------------------------
+# parse_arguments()
+#-------------------------------------------------------------------------------
 
 def parse_arguments():
     """
@@ -46,16 +51,13 @@ def parse_arguments():
         Create target windows using target annotations and parsed Prokka output.
         """)
         
-
     # Required arguments
     required_args = parser.add_argument_group('Required')
-    # Parse GBK file
     required_args.add_argument('-r', '--report_file', type = str, required = True,
         help = """
         Path for exporting data as a new TSV report.
         Example: 'path/new.tsv'
         """)
-    # Create windows
     required_args.add_argument('-p', '--prokka', type = str, required = True,
         help = """
         Path to a TSV file produced by parse_gbk.py.
@@ -154,9 +156,6 @@ def parse_arguments():
         action='version',
         version=f'%(prog)s commit {__version__}'
         )
-    
-    
-    # Vis windows
     optional_args.add_argument('-pile', '--pile', action='store_true',
         help = """
         Save figure in strandpile format
@@ -168,77 +167,65 @@ def parse_arguments():
         parser.print_help()
         sys.exit(1)
 
-    # Get the arguments into an object
     args = parser.parse_args()
     
     logging.basicConfig(level=args.verbosity.upper())
 
     return args
 
-
-def main(args):
-    """
-    Run the three scripts in the workflow
-
-    :param args: Argument parser object with required arguments
-    """
-    logging.info('Validating arguments')
-    # Ensure that the supplied arguments are valid
-    parse_gbk_check(args)
-
-    # Parse the supplied GBK file
-    parse_gbk_main(args)
-
-    # Create windows
-    create_windows_main(args)
-    
-    # Create figure
-    call('vis_windows.R')
+#-------------------------------------------------------------------------------
+# Check functions
+#-------------------------------------------------------------------------------
 
 def parse_gbk_check(args):
     """
-    Perform checks to ensure that the supplied arguments are valid
+    Perform checks to ensure that the supplied arguments are valid.
 
-    :param args: Argument parser object with required arguments
+    :param args: Argument parser object with required arguments.
     """
+
     # List to store all the errors encountered
     errors = []
+
     # Input
     if args.input:
         if not os.path.isfile(args.input):
-            errors.append(
-                    f'Could not locate the supplied GBK file, {args.input}. '
-                    'Please ensure that it exists.'
-                )
+            errors.append(f"""
+            Could not locate the supplied GBK file, {args.input}. Please ensure
+            that it exists.            
+            """)
     elif args.dir:
         if not os.path.isdir(args.dir):
-            errors.append(
-                f'Could not locate the supplied directory of GBK files, '
-                f'{args.dir}. Please ensure that it exists.'
-            )
+            errors.append(f"""
+            Could not locate the supplied directory of GBK files, {args.dir}.
+            Please ensure that it exists.
+            """)
         else:
             gbk_files = []
             for path in Path(args.dir).rglob('*.gbk'):
                 gbk_files.append(path)
             if not gbk_files:
-                errors.append(
-                    f'Could not locate any GBK files in the supplied directory, '
-                    f'{args.dir}. Please ensure that you have supplied the right folder name.'
-                )
+                errors.append(f"""
+                Could not locate any GBK files in the supplied directory, 
+                {args.dir}. Please ensure that you have supplied the correct 
+                folder name.
+                """)
+
     # Report file
     report_path = os.path.basename(args.report)
     if not os.path.isdir(report_path):
         try:
             os.makedirs(report_path)
-            logging.warning(
-                'Directory for supplied report file, %s did not exist. It has '
-                'been created for you.', args.report
-                )
+            logging.warning(f"""
+            Directory for supplied report file, %s did not exist. It has been
+            created for you: {args.report}
+            """)
         except IOError as exc:
-            errors.append(
-                f'Could not create the necessary folder into which the report is to be written: ' 
-                f' {report_path}. Please ensure that you have the necessary permissions.\n'
-            )
+            errors.append(f"""
+            Could not create the necessary folder into which the report is to be
+            written: {report_path}. Please ensure that you have the necessary 
+            permissions.\n
+            """)
             logging.debug(exc)
     if errors:
         error_writing(errors)
@@ -249,13 +236,40 @@ def error_writing(errors):
 
     :param errors: List of errors encountered with the arguments
     """
+
     error_string = '\n'.join(errors)
     was_were = 'was' if len(errors) == 1 else 'were'
     correct = 'error' if len(errors) == 1 else 'errors'
-    logging.error(
-        'There %s %s %s when attempting to run your command: \n%s', 
-        was_were, len(errors), correct, error_string)
+    logging.error(f"""
+    There {was_were} {len(errors)} {correct} when attempting to run your
+    command: \n{error_string}
+    """)
+
     raise SystemExit
+
+#-------------------------------------------------------------------------------
+# main()
+#-------------------------------------------------------------------------------
+
+def main(args):
+    """
+    Run the three scripts in the workflow.
+
+    :param args: Argument parser object with required arguments.
+    """
+
+    logging.info('Validating arguments')
+
+    # Ensure that the supplied arguments are valid
+    parse_gbk_check(args)
+    # Parse the supplied GBK file
+    parse_gbk_main(args)
+    # Create windows
+    create_windows_main(args) 
+    # Create figure
+    call('vis_windows.R')
+
+#-------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     args = parse_arguments()
